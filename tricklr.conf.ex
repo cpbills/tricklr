42fa7ea6895a431e7a3be6844d83209ec662df5d
Christopher P. Bills <cpbills@fauxtographer.net>
2012-01-29 08:42:57 -0800

    it works. hooray...
    - added the ability to authenticate and then verify authentication
    - added output to update your config file, and so on...
    - currently doesn't 'support' all of the parameters you can send the
      upload URL, but it should be a fairly quick fix...
    - removed references to 'frob' which is only actually used for getting
      the auth key, never needed again...

---
+++ tricklr.conf.ex
@@ -0,0 +1,10 @@
+api_key     get an api_key and secret here:
+secret      http://www.flickr.com/services/apps/create/apply
+
+public      0
+friends     0
+family      0
+
+min         1
+max         5
+src_dir     /home/user/files/image-pool
--- tricklr.pl
+++ tricklr.pl
@@ -21,6 +21,7 @@ use warnings;
 # a script to use with something else you have to grab custom from CPAN when
 # it's just a wrapper around LWP::UserAgent and HTTP::Request::Common?
 use LWP::UserAgent;
+use HTTP::Request;
 use HTTP::Request::Common 'POST';
 use XML::Simple;
 use Getopt::Std;
@@ -55,48 +56,256 @@ $$options{verbose} = $cli_opts{v} if ($cli_opts{v});
 
 my $min = 1;
 my $max = 1;
-# i don't usually do 'defined' but the value could be 0
+# i don't usually do 'defined', but the value /could/ be 0
 $min = $$options{min} if (defined $$options{min});
 $max = $$options{max} if (defined $$options{max});
+
 # just in case max/min get switched or something, user error...?
 if ($min > $max) {
     my $temp = $max;
     $max = $min;
     $min = $temp;
 }
+
+# generate a random number from $min to $max to use
 my $number = (int(rand($max-$min))+$min)-1;
-my $ua = new LWP::UserAgent;
 
-
-
-opendir DIR,"$$options{src_dir}";
-my @files = grep { !/^\.\.?$/ } readdir DIR;
-closedir DIR;
-
-fisher_yates_shuffle(@files);
-$number = $#files if ($number > $#files);
-foreach my $file (@files[0 .. $number]) {
-    if (upload_to_flickr($options,"$$options{src_dir}/$file",$ua)) {
-        unlink "$$options{src_dir}/$file";
-    }
+if ($$options{auth_token}) {
+    # this may re-set the auth token if they are invalid;
+    # so we can't do an 'else', as the test value may change
+    verify_auth_token($options);
 }
+
+unless ($$options{auth_token}) {
+    $$options{frob} = get_frob($options);
+    print_auth_url($options);
+    $$options{auth_token} = get_auth_token($options);
+    print "please update your configuration with:\n\n";
+    print "auth_token  $$options{auth_token}\n\n";
+}
+
+if (opendir DIR,"$$options{src_dir}") {
+    my @files = grep { !/^\.\.?$/ } readdir DIR;
+    closedir DIR;
+
+    fisher_yates_shuffle(\@files);
+    $number = $#files if ($number > $#files);
+    foreach my $file (@files[0 .. $number]) {
+        unless (upload_to_flickr($options,"$$options{src_dir}/$file")) {
+            #unlink "$$options{src_dir}/$file";
+        }
+    }
+} else {
+    print STDERR "unable to open $$options{src_dir}: $!\n";
+    exit 1;
+}
+
 exit;
 
 sub upload_to_flickr {
     my $options     = shift;
     my $filename    = shift;
-    my $ua          = shift;
 
     my $url = 'http://api.flickr.com/services/upload/';
 
-    my $req = POST $url,
+    my $args = {
+        api_key         => $$options{api_key},
+        auth_token      => $$options{auth_token},
+        is_public       => $$options{public},
+        is_friend       => $$options{friends},
+        is_family       => $$options{family}
+    };
+
+    my $sig = $$options{secret};
+    foreach my $key (sort {$a cmp $b} keys %$args) {
+        my $value = (defined($$args{$key})) ? $$args{$key} : "";
+        $sig .= $key . $value;
+    }
+    $sig = md5_hex($sig);
+
+    my $browser = new LWP::UserAgent;
+       $browser->timeout(10);
+       $browser->requests_redirectable(['POST','GET','HEAD']);
+    my $request = POST $url,
         Content_Type => 'form-data',
         Content => [
-            is_public   => $$options{public},
-            is_friend   => $$options{friends},
-            is_family   => $$options{family},
-            photo       => [ "$filename" ]
+            %{$args},
+            api_sig => $sig,
+            photo   => [ "$filename" ]
         ];
+    my $response = $browser->request($request);
+    if ($response->is_success) {
+        my $xml = new XML::Simple;
+        my $data = $xml->XMLin($response->content);
+        if ($$data{stat} eq 'ok') {
+            if ($$options{verbose}) {
+                print "uploaded $filename - photoid $$data{photoid}\n";
+            }
+            return 0;
+        } else {
+            print STDERR "\'$$data{err}{msg}\' when uploading $filename\n";
+            return 1;
+        }
+    } else {
+        print STDERR "failed to upload image $filename";
+        return 1;
+    }
+    # shouldn't make it here...
+    return 1;
+}
+
+sub print_auth_url {
+    # prints the URL a user needs to enter to authorize this script
+    # for write permission to their flickr stream.
+    my $options = shift;
+
+    my $args = {
+        api_key => $$options{api_key},
+        frob    => $$options{frob},
+        perms   => 'write'
+    };
+
+    my $sig = sign_request($options,$args);
+    my $url = "http://flickr.com/services/auth/?$sig";
+
+    print "please browse to: $url\n";
+    print "then hit [enter]";
+    my $dummy = <STDIN>;
+}
+
+sub verify_auth_token {
+    # verify that the auth token is valid and grants write access
+    # and delete the stored value if the key proves to be invalid
+    my $options = shift;
+
+    my $args = {
+        api_key     => $$options{api_key},
+        auth_token  => $$options{auth_token},
+        method      => 'flickr.auth.checkToken'
+    };
+
+    my $sig = sign_request($options,$args);
+    my $url = "http://api.flickr.com/services/rest/?$sig";
+
+    my $content = http_get($url);
+    if ($content) {
+        my $xml = new XML::Simple;
+        my $data = $xml->XMLin($content);
+        if ($$data{stat} eq 'ok') {
+            if ($$data{auth}{perms} ne 'write') {
+                print STDERR "no write permission for provided auth_token\n";
+                delete $$options{auth_token};
+            }
+            return;
+        } else {
+            if ($$data{err}{code} == 98) {
+                print STDERR "$$data{err}{msg} - reset auth tokens\n";
+                delete $$options{auth_token};
+                return;
+            } else {
+                print STDERR "$$data{err}{msg}\n";
+                exit $$data{err}{code};
+            }
+        }
+    } else {
+        # we don't really want to clobber the token if the page is simply
+        # unreachable, or the request fails for some other reason...
+        print STDERR "failed to verify token one way or another\n";
+        exit 1;
+    }
+}
+
+sub get_auth_token {
+    # returns the auth key once an account has been authenticated via
+    # the web browser and granting the app permission...
+    my $options = shift;
+
+    my $args = {
+        api_key => $$options{api_key},
+        frob    => $$options{frob},
+        method  => 'flickr.auth.getToken'
+    };
+
+    my $sig = sign_request($options,$args);
+    my $url = "http://api.flickr.com/services/rest/?$sig";
+
+    my $content = http_get($url);
+    if ($content) {
+        my $xml = new XML::Simple;
+        my $data = $xml->XMLin($content);
+        if ($$data{stat} eq 'ok') {
+            return $$data{auth}{token};
+        } else {
+            if ($$data{err}{code} == 108) {
+                print STDERR "$$data{err}{msg}: did you browse to the url?\n";
+            } else {
+                print STDERR "$$data{err}{msg}\n";
+            }
+            exit $$data{err}{code};
+        }
+    } else {
+        print STDERR "failed to get auth key\n";
+        exit 1;
+    }
+}
+
+sub get_frob {
+    # gets the 'frob' (what the fuck is a frob?) from flickr which is part
+    # of the authentication process and needed to upload images.
+    my $options = shift;
+
+    my $args = {
+        api_key => $$options{api_key},
+        method  => 'flickr.auth.getFrob'
+    };
+
+    my $sig = sign_request($options,$args);
+    my $url = "http://api.flickr.com/services/rest/?$sig";
+
+    my $content = http_get($url);
+    if ($content) {
+        my $xml = new XML::Simple;
+        my $data = $xml->XMLin($content);
+        if ($$data{stat} eq 'ok') {
+            return $$data{frob};
+        } else {
+            print STDERR "$$data{err}{msg}\n";
+            exit $$data{err}{code};
+        }
+    } else {
+        print STDERR "failed to get frob\n";
+        exit 1;
+    }
+}
+
+sub http_get {
+    my $url     = shift;
+
+    my $browser = new LWP::UserAgent;
+       $browser->timeout(10);
+       $browser->requests_redirectable(['POST','GET','HEAD']);
+    my $request = new HTTP::Request('GET',"$url");
+    my $response = $browser->request($request);
+
+    if ($response->is_success) {
+        return $response->content;
+    }
+    return undef;
+}
+
+sub sign_request {
+    my $options = shift;
+    my $args    = shift;
+
+    my @params = ();
+    my $sig = $$options{secret};
+    foreach my $key (sort {$a cmp $b} keys %$args) {
+        my $value = (defined($$args{$key})) ? $$args{$key} : "";
+        $sig .= $key . $value;
+        push @params,"$key=$value";
+    }
+    return join('&',@params) . '&api_sig=' . md5_hex($sig);
+}
 
 sub fisher_yates_shuffle {
     my $array = shift;
